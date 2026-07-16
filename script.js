bf8a776aab7b9bf7218ad220b1841ee0772eb7ab
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-16 10:34:31 -0700

    Add event listener to button
    added logic to take input from user when button
       is entered
    logic will check if input is a number and is in
       the range of 1-100

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Elements
 const container = document.querySelector("#container");
 const containerChildren = document.querySelectorAll("#container > div");
+const gridSizeInput = document.querySelector("#grid-size-input");
+const gridSizeButton = document.querySelector("#grid-size-button");
 
 // Loop to create 16 by 16 div container
 for (let i = 0; i < 16 ** 2; i++) {
@@ -18,3 +20,18 @@ container.addEventListener(
   "mouseover",
   (e) => (e.target.style["background-color"] = "black"),
 );
+
+// event listener listens process input when button is clicked on
+// will check if input is 1-100 and is a number
+gridSizeButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = +gridSizeInput.value;
+  if (!isNaN(inputValue)) {
+    inputValue >= 1 && inputValue <= 100
+      ? alert(`Number entered: ${inputValue}`)
+      : alert("Invalid input. Please enter a number from 1-100.");
+  } else {
+    alert("Invalid input. Please enter a number.");
+  }
+  gridSizeButton.focus();
+});
