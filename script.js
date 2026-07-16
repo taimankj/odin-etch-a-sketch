60141debe5451d554396063ed816440ab1a69ded
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-15 19:54:02 -0700

    compiled class adding logic to container

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Elements
 const container = document.querySelector("#container");
+const containerChildren = document.querySelectorAll("#container > div");
 
 // Loop to create 16 by 16 div container
 for (let i = 0; i < 16 ** 2; i++) {
@@ -8,11 +9,6 @@ for (let i = 0; i < 16 ** 2; i++) {
   newDiv.style.width = newDivSize;
   newDiv.style.height = newDivSize;
   newDiv.style["flex"] = `0 0 ${newDivSize}`;
+  newDiv.classList.add("container-child");
   container.appendChild(newDiv);
 }
-
-// Add padding and margin to all divs
-const containerChildren = document.querySelectorAll("#container > div");
-containerChildren.forEach((child) => {
-  child.classList.add("container-child");
-});
