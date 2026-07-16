3ed83c21c9c814d46256bef316b654b099722d78
Keoni Taimanglo <taimanglo.oni@hotmail.com>
2026-07-16 10:57:26 -0700

    Add logic to create grid
    function, createGrid(), made to create grid based
       off user input
    if grid exists, original grid will be deleted.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // Elements
 const container = document.querySelector("#container");
-const containerChildren = document.querySelectorAll("#container > div");
 const gridSizeInput = document.querySelector("#grid-size-input");
 const gridSizeButton = document.querySelector("#grid-size-button");
 
-// Loop to create 16 by 16 div container
-for (let i = 0; i < 16 ** 2; i++) {
-  let newDiv = document.createElement("div");
-  let newDivSize = Math.floor(960 / 16) + "px";
-  newDiv.style.width = newDivSize;
-  newDiv.style.height = newDivSize;
-  newDiv.style["flex"] = `0 0 ${newDivSize}`;
-  newDiv.classList.add("container-child");
-  container.appendChild(newDiv);
+// function will create grid size based off input given
+function createGrid(gridSize) {
+  const containerChildren = document.querySelectorAll("#container > div");
+  // delete current grid if there is one
+  if (containerChildren.length > 0) {
+    for (const child of containerChildren) {
+      child.remove();
+    }
+  }
+
+  for (let i = 0; i < gridSize ** 2; i++) {
+    let newDiv = document.createElement("div");
+    let newDivSize = Math.floor(960 / gridSize) + "px";
+
+    newDiv.style.width = newDivSize;
+    newDiv.style.height = newDivSize;
+    newDiv.style["flex"] = `0 0 ${newDivSize}`;
+    newDiv.classList.add("container-child");
+
+    container.appendChild(newDiv);
+  }
 }
 
 // event listener listens to mouse hover actions on div elements and changes the background-color when event fires (event bubbling makes this happen)
@@ -25,13 +36,15 @@ container.addEventListener(
 // will check if input is 1-100 and is a number
 gridSizeButton.addEventListener("click", (e) => {
   e.preventDefault();
+
   let inputValue = +gridSizeInput.value;
   if (!isNaN(inputValue)) {
     inputValue >= 1 && inputValue <= 100
-      ? alert(`Number entered: ${inputValue}`)
+      ? createGrid(inputValue)
       : alert("Invalid input. Please enter a number from 1-100.");
   } else {
     alert("Invalid input. Please enter a number.");
   }
-  gridSizeButton.focus();
+
+  gridSizeInput.value = "";
 });
